cb9608ec562ee1376baed3d65367dd9873c998dd
zexpher <fahriangga30@gmail.com>
2023-10-03 23:39:54 +0700

    Contoh1 penggunaan hanya controller

---
+++ pertemuan2/app/Controllers/Latihan1.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Latihan1 extends BaseController
+{
+    public function index()
+    {
+        echo "Selamat Datang.. selamat belajar Web Programming II";
+    }
+}
