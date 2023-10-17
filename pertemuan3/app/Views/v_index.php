bb1144646843ea211082f0a42feb4318a01f1090
zexpher <fahriangga30@gmail.com>
2023-10-17 11:02:07 +0700

    Upload file views

---
+++ pertemuan3/app/Views/v_index.php
@@ -0,0 +1,7 @@
+<?= $this->extend('v_layout') ?> <!-- Mengambil template dari v_layout.php -->
+
+<?= $this->section('content') ?> <!-- Mengambil content dari v_layout.php -->
+
+
+
+<?= $this->endSection() ?> <!-- Mengakhiri content dari v_layout.php -->
---
+++ pertemuan3/app/Views/v_layout.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Web Prog II | Merancang Template sederhana dengan codeigniter</title>
+    <link rel="stylesheet" type="text/css" href="<?= base_url("assets/css/stylebuku.css") ?>">
+</head>
+
+<body>
+    <?= $this->include('v_header') ?> <!-- Memasukkan file v_header.php untuk Header -->
+
+    <?= $this->renderSection('section') ?> <!-- Memasukkan isi content yang ada di v_index.php -->
+
+    <?= $this->include('v_footer') ?> <!-- Memasukkan file v_footer.php untuk Footer -->
+</body>
+
+</html>
