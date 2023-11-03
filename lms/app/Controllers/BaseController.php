6e7cbe9e1459b69f074e6ad07cb57be19552a0f1
zexpher <fahriangga30@gmail.com>
2023-11-03 16:52:27 +0700

    Membuat helper menu

--- lms/app/Controllers/BaseController.php
+++ lms/app/Controllers/BaseController.php
@@ -35,7 +35,7 @@ abstract class BaseController extends Controller
      *
      * @var array
      */
-    protected $helpers = ['helper'];
+    protected $helpers = ['menu'];
 
     /**
      * Be sure to declare properties for any property fetch you initialized.
