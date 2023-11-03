bd48776ba9536d5d263bd0d5b7992b474597f78e
zexpher <fahriangga30@gmail.com>
2023-11-03 17:00:42 +0700

    menyesuaikan menu pada role admin

--- lms/app/Views/layout-sub/sidebar/admin.php
+++ lms/app/Views/layout-sub/sidebar/admin.php
@@ -8,3 +8,50 @@
         </p>
     </a>
 </li>
+
+<li class="nav-item">
+    <a href="#" class="nav-link <?= $menu === 'profile' ? 'active' : '' ?>">
+        <i class="nav-icon fas fa-user"></i>
+        <p>
+            Profile
+        </p>
+    </a>
+</li>
+
+<li class="nav-header">Master Data</li>
+
+<li class="nav-item">
+    <a href="#" class="nav-link <?= $menu === 'kelas' ? 'active' : '' ?>">
+        <i class="nav-icon fas fa-chalkboard-teacher"></i>
+        <p>
+            Kelas
+        </p>
+    </a>
+</li>
+
+<li class="nav-item">
+    <a href="#" class="nav-link <?= $menu === 'mapel' ? 'active' : '' ?>">
+        <i class="nav-icon fas fa-calendar-alt"></i>
+        <p>
+            Jadwal
+        </p>
+    </a>
+</li>
+
+<li class="nav-item">
+    <a href="#" class="nav-link <?= $menu === 'siswa' ? 'active' : '' ?>">
+        <i class="nav-icon fas fa-user-graduate"></i>
+        <p>
+            Siswa
+        </p>
+    </a>
+</li>
+
+<li class="nav-item">
+    <a href="#" class="nav-link <?= $menu === 'guru' ? 'active' : '' ?>">
+        <i class="nav-icon fas fa-user-tie"></i>
+        <p>
+            Guru
+        </p>
+    </a>
+</li>
