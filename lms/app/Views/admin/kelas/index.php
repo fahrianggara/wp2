19c657a42d82da4b924bbb529fdd090354e85ed1
zexpher <fahriangga30@gmail.com>
2023-11-05 15:06:53 +0700

    Menampilkan data kelas

--- lms/app/Views/admin/kelas/index.php
+++ lms/app/Views/admin/kelas/index.php
@@ -4,7 +4,7 @@
 
 <div class="container-fluid">
     <div class="row align-items-center justify-content-center">
-        <div class="col-xl-8 col-lg-12 col-md-12">
+        <div class="col-lg-12">
 
             <div class="card">
                 <div class="card-header p-2">
@@ -22,12 +22,52 @@
                                 <tr>
                                     <th>#</th>
                                     <th>Kelas</th>
+                                    <th>Daftar Siswa</th>
                                     <th>&nbsp;</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <?php $no = 1 ?>
-                                
+                                <?php foreach($classrooms as $classroom): ?>
+                                    <?php 
+                                        $users = $db->table('students')
+                                            ->join('classrooms', 'classrooms.id = students.classroom_id', 'LEFT')
+                                            ->join('users', 'users.id = students.user_id', 'LEFT')
+                                            ->select('users.first_name, users.id_number as number', 'students.classroom_id')
+                                            ->where('classroom_id', $classroom->id)
+                                            ->get()->getResult();
+                                    ?>
+
+                                    <tr>
+                                        <td><?= $no++ ?></td>
+                                        <td><?= strtoupper($classroom->name) ?></td>
+                                        <td>
+                                            <?php foreach($users as $user): ?>
+                                                <span class="badge badge-primary">
+                                                    <?= "$user->first_name ($user->number)" ?>
+                                                </span>
+                                            <?php endforeach ?>
+                                        </td>
+                                        <td>
+                                            <div class="btn-group dropleft">
+                                                <button class="btn btn-sm btn-more dropdown-toggle"
+                                                    data-toggle="dropdown" aria-expanded="false" data-display="static">
+                                                    <i class="fas fa-ellipsis-v"></i>
+                                                </button>
+                                                <div class="dropdown-menu dropdown-menu-right">
+                                                    <a href="<?= base_url("admin/kelas/edit/" . base64_encode($classroom->id)) ?>" class="dropdown-item py-1">
+                                                        <i class="fas text-warning fa-pen mr-2"></i> Edit
+                                                    </a>
+                                                    <button type="button" value="<?= base64_encode($classroom->id) ?>" class="dropdown-item py-1 btn-delete"
+                                                        data-name="<?= strtoupper($classroom->name) ?>"
+                                                        data-action="<?= route_to('admin.kelas.destroy') ?>">
+                                                        <i class="fas text-danger fa-trash mr-2"></i> Hapus
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                <?php endforeach ?>
                             </tbody>
                         </table>
                     </div>
