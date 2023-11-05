c7ea09bcd4718b49f0f04dcfce45b97ea4fb2bbf
zexpher <fahriangga30@gmail.com>
2023-11-05 16:09:34 +0700

    ketinggalan ganti ClassRoom jadi Classroom

--- lms/app/Database/Seeds/ClassRoomSeeder.php
+++ lms/app/Database/Seeds/ClassRoomSeeder.php
@@ -2,34 +2,34 @@
 
 namespace App\Database\Seeds;
 
-use App\Models\ClassRoomModel;
+use App\Models\ClassroomModel;
 use CodeIgniter\Database\Seeder;
 
-class ClassRoomSeeder extends Seeder
+class ClassroomSeeder extends Seeder
 {
     public function run()
     {
-        $classroomModel = new ClassRoomModel();
+        $classroomModel = new ClassroomModel();
 
         $classroomModel->insertBatch([
             [
-                'name' => '10.1A.01',
+                'name' => '10.1a.01',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s'),
             ], [
-                'name' => '10.1A.02',
+                'name' => '10.1a.02',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s'),
             ], [
-                'name' => '10.2A.01',
+                'name' => '10.2a.01',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s'),
             ], [
-                'name' => '10.2A.02',
+                'name' => '10.2a.02',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s'),
             ], [
-                'name' => '10.3A.01',
+                'name' => '10.3a.01',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s'),
             ]
